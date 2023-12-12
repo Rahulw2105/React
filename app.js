ff42cfd3ddb9d1f679105bbbf9d9b37dc34dd246
Rahulw2105 <wankhederahul116@gmail.com>
2023-12-12 20:35:18 +0530

    funnctional component

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" },
-  "Hello world! from React"
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-console.log(heading);
+const FirstComponent = () => {
+  return (
+    <div>
+      <h1> First Component </h1>
+    </div>
+  );
+};
+
+const SecondComp = () => {
+  return (
+    <div>
+      <FirstComponent />
+      <h1> Second component </h1>
+    </div>
+  );
+};
+
+const jsxHeading = <h1> This is jsx heading</h1>;
+console.log(jsxHeading);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(<SecondComp />);
